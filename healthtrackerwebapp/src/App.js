06437d414063c5934100942077826d6304677a38
chonger878 <echong06@gmail.com>
2022-10-07 01:19:10 -0700

    Added new links

--- healthtrackerwebapp/src/App.js
+++ healthtrackerwebapp/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, Outlet} from "react-router-dom"
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to='/about'>About Us</Link>
+        <Link to='/contact'>Contact Us</Link>
+      </nav>
+      <h1>Rare Diseases/Disorders Tracker App</h1>
     </div>
   );
 }
 
-export default App;
+export function App(){
+  return <Home />
+};
